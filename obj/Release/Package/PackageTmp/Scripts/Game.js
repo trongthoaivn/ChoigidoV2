313404d725573535f7be754a024c5059a0ace964
trongthoaivn <52898471+trongthoaivn@users.noreply.github.com>
2021-07-12 14:15:59 +0700

    #update 5

--- obj/Release/Package/PackageTmp/Scripts/Game.js
+++ obj/Release/Package/PackageTmp/Scripts/Game.js
@@ -6,6 +6,7 @@ var searchParams = new URLSearchParams(window.location.search);
 var param = searchParams.get('Id');
 $(document).ready(function () {
 
+
     //popup lên thông báo nhập tên
     var person = prompt("Hãy nhập tên của bạn", "");
     if (person == "") {
@@ -73,11 +74,44 @@ $(document).ready(function () {
         } else $("#kayle").text("Opponent's turn !");
     }
 
+    hub.client.setUpdateBoard = function (fen) {
+        Clear_board();
+        draw_pieces(fen);
+        if (turn == "white") set_drag("w")
+        else set_drag("b")
+        if (turn == playeris) {
+            $("#kayle").text("Your turn !");
+        } else $("#kayle").text("Opponent's turn !");
+    }
+
+    // request undo
     hub.client.getReply = function (msg, fen) {
-         console.log("aa")
-        var reply = confirm("The opponent asked to be undo " + msg);
-       
-        //alert(reply);
+        if (fen == "") {
+            $.alert({
+                title: 'Reply for request undo',
+                content: 'Rejected',
+            });
+        } else
+            $.confirm({
+            title: 'Notification',
+            content: 'The opponent asked to be undo ' + msg,
+            buttons: {
+                Accept: {
+                    text: 'Accept',
+                    btnClass: 'btn-green',
+                    action: function () {
+                        hub.server.sendUpdateBoard(param, fen);
+                    }
+                },
+                Ignore: {
+                    text: 'Rejected',
+                    btnClass: 'btn-red',
+                    action: function () {
+                        hub.server.sendRequest(param, msg, "");
+                    }
+                }
+            }
+        });
     }
 
     // play để bắt đầu
@@ -86,8 +120,10 @@ $(document).ready(function () {
             set_drag("w");
             playeris = "white";
             $("#kayle").text("Your turn fisrt !");
-           // hub.server.set_turn(param,"w")
-        } else {
+            // hub.server.set_turn(param,"w")
+
+        } else
+        if (player_name == $('#BlackId').text()){
             set_drag("b");
             playeris = "black";
             $("#kayle").text("Opponent's turn !");
@@ -199,7 +235,7 @@ const R = new pieces("R", "w", "/Content/Image/white_rook.svg", "");
 const P = new pieces("P", "w", "/Content/Image/white_pawn.svg", "");
 
 const List_pieces = [k, q, b, n, r, p, K, Q, B, N, R, P]
-var turn = "white";
+var turn ="white" ;
 //tạo bàn cờ theo fen
 function Create_lick() {
     var Fen = $("#Fen").val();
@@ -233,11 +269,14 @@ function fen_generate() {
             str = str.slice(0, -1)
         }
     }
+    let color = (turn == "white") ? "w" : "b";
+    fen = fen.slice(0, -1) + " " + color;
     return fen;
 }
 //clear board
 function Clear_board() {
-    $('img').remove();
+    $('.b').remove();
+    $('.w').remove();
 }
 // tạo quân cờ
 function draw_pieces(Fen) {
@@ -375,9 +414,9 @@ function set_drop(id) {
 }
 var init = 1;
 function createLog(ol, ne, fen) {
-    $('#move-table').append(' <button type="button" style="text-align:center" id="' + fen + '" class="list-group-item ">' + init + ' : ' + ol + ' => ' + ne + ' </button>');
-    $("#move-table button").click(function () {
-        hub.server.sendRequest(param, init + ' : ' + ol + ' => ' + ne, fen);
+    $('#move-table').append(' <button type="button" style="text-align:center" id="' + fen + '" class="Log' + init + ' list-group-item ">' + init + ' : ' + ol + ' => ' + ne + ' </button>');
+    $('.Log' + init).click(function () {
+        hub.server.sendRequest(param, $(this).text(), $(this).attr("id"));
     });
     init++;
 }
